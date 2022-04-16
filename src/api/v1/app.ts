97fa3063519dec78558f85e1d9ef689c11c57157
AlanFnz <alan.f@msn.com>
2022-04-16 01:43:52 -0300

    file formatting

--- src/api/v1/app.ts
+++ src/api/v1/app.ts
@@ -3,8 +3,10 @@ import * as http from "http";
 import * as winston from "winston";
 import * as expressWinston from "express-winston";
 import cors from "cors";
+
 import { CommonRoutesConfig } from "./common/common.routes.config";
 import { UsersRoutes } from "./users/users.routes.config";
+
 import debug from "debug";
 
 const app: express.Application = express();
