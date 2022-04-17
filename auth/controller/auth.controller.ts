7eee0643b98d6f991a7ea4763ddf87e0727407c4
AlanFnz <alan.f@msn.com>
2022-04-17 15:49:11 -0300

    moved declaration

--- auth/controller/auth.controller.ts
+++ auth/controller/auth.controller.ts
@@ -5,13 +5,13 @@ import crypto from "crypto";
 
 const log: debug.IDebugger = debug("app:auth-controller");
 
-// @ts-expect-error
-const jwtSecret: string = process.env.JWT_SECRET;
 const tokenExpirationInSeconds = 36000; // TODO: move to config
 
 class AuthController {
   async createJWT(req: express.Request, res: express.Response) {
     try {
+      // @ts-expect-error
+      const jwtSecret: string = process.env.JWT_SECRET;
       const refreshId = req.body.userId + jwtSecret;
       const salt = crypto.createSecretKey(crypto.randomBytes(16));
       const hash = crypto
