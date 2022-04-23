c90782eaa8261af34f0fad0bab5ecbb51847ce63
AlanFnz <alan.f@msn.com>
2022-04-22 21:04:59 -0300

    created method on users middleware

--- users/middleware/users.middleware.ts
+++ users/middleware/users.middleware.ts
@@ -67,6 +67,23 @@ class UsersMiddleware {
     req.body.id = req.params.userId;
     next();
   }
+
+  async userCantChangePermission(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    if (
+      "permissionFlags" in req.body &&
+      req.body.permissionFlags !== res.locals.user.permissionFlags
+    ) {
+      res.status(400).send({
+        errors: ["User cannot change permission flags"],
+      });
+    } else {
+      next();
+    }
+  }
 }
 
 export default new UsersMiddleware();
