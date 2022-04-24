9ad90dd76d2654eeb7f32c36e512d541198310fc
AlanFnz <alan.f@msn.com>
2022-04-24 19:25:11 -0300

    implemented config service

---
+++ common/config/dao/config.dao.ts
@@ -0,0 +1,68 @@
+import debug from "debug";
+import { Types } from 'mongoose';
+
+import { CreateConfigDto } from "../dto/create.config.dto";
+import { PatchConfigDto } from "../dto/patch.config.dto";
+
+import mongooseService from "../../services/mongoose.service";
+
+const log: debug.IDebugger = debug("app:config-dao");
+
+class ConfigDao {
+  Schema = mongooseService.getMongoose().Schema;
+
+  configSchema = new this.Schema(
+    {
+      minimumAppVersion: String,
+      features: [String], // TODO: update this one with reference to Feature object
+      languages: [String],
+      privacyPolicy: String,
+      termsAndConditions: String,
+    },
+    {
+      collection: "config",
+    }
+  );
+
+  Config = mongooseService.getMongoose().model("Config", this.configSchema);
+
+  constructor() {
+    log("Created new instance of ConfigDao");
+  }
+
+  async addConfig(configFields: CreateConfigDto) {
+    //// FIXME: move to middleware validation ////
+    const count = await this.Config.countDocuments();
+    if (count > 0) return; 
+    //////////////////////////////////////////////
+
+    const config = new this.Config({
+      ...configFields,
+    });
+
+    await config.save();
+    return config;
+  }
+
+  async getConfig() {
+    return this.Config.find({}).exec();
+  }
+
+  async updateConfig(configFields: PatchConfigDto) {
+    const config = await this.Config.find({});
+    console.log(config);
+/*     const existingUser = await this.Config.findOneAndUpdate(
+      { _id: userId },
+      { $set: configFields },
+      { new: true }
+    ).exec(); */
+
+    return;
+  }
+
+  async removeConfig(configId: Types.ObjectId) {
+    return this.Config.deleteOne({ _id: configId }).exec();
+  }
+}
+
+export default new ConfigDao();
---
+++ common/config/dto/patch.config.dto.ts
@@ -0,0 +1,7 @@
+export interface PatchConfigDto {
+  minimumAppVersion?: string;
+  features?: string;
+  languages?: string;
+  privacyPolicy?: string;
+  termsAndConditions?: string;
+}
