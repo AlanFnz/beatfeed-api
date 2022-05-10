3aa060a567c133a2271bedd380454b1058091efe
AlanFnz <alan.f@msn.com>
2022-05-10 16:45:28 -0300

    moved config routes to constants

--- common/constants/endpoints.js
+++ common/constants/endpoints.js
@@ -1,3 +1,6 @@
+//////// CONFIG ////////
+const CONFIG_BASE = "/config";
+const PARAM_CONFIG_ID = "/:configId";
 //////// USERS /////////
 const USERS_BASE = "/users";
 const PARAM_USER_ID = "/:userId";
@@ -14,4 +17,9 @@ export const USERS = {
   USER_ID: USERS_BASE + PARAM_USER_ID,
   USER_PERMISSION_FLAGS:
     USERS_BASE + PARAM_USER_ID + PERMISSION_FLAGS + PARAM_PERMISSION_FLAGS,
+};
+
+export const CONFIG = {
+  CONFIG: CONFIG_BASE,
+  CONFIG_ID: CONFIG_BASE + PARAM_CONFIG_ID,
 };
