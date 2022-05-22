fa15d5b066c2e91bb0a3f64a08b9ab2a1800515f
AlanFnz <alan.f@msn.com>
2022-05-22 16:55:03 -0300

    created response messages object

---
+++ common/constants/responseMessages.constants.ts
@@ -0,0 +1,27 @@
+export const ResponseMessages = {
+  // common
+  VALIDATION_ERROR:
+    "One or more errors occurred when validating this request body",
+  OBJECT_ID_ERROR: "Something went wrong when getting this object id",
+  // auth
+  JWT_FAIL: "Something went wrong when creating this token",
+  JWT_REFRESH_INVALID: "Invalid refresh token",
+  JWT_REFRESH_MISSING: "Missing required field: refreshToken",
+  INVALID_DATA: "Invalid email and/or password",
+  // permissions
+  FORBIDDEN: "This user doesn't have the required permissions",
+  PERMISSION_PARSING_ERROR:
+    "Something went wrong when parsing this user's permissions",
+  USER_CANNOT_CHANGE_PERMISSIONS: "User cannot change permission flags",
+  // config
+  CONFIG_EXISTENT: "A config object already exists",
+  // features
+  FEATURE_NOT_FOUND: (featureId: any) => `Feature ${featureId} not found`,
+  // users
+  USER_CREATE_FAIL: "Something went wrong when creating this user",
+  USER_EMAIL_EXISTS: "User email already exists",
+  USER_EMAIL_INVALID: "Invalid email",
+  USER_NOT_FOUND: (userId: any) => `User ${userId} not found`,
+  USER_LAST_LOGIN_UPDATE_ERROR:
+    "Something went wrong when updating this user's last login date",
+};
