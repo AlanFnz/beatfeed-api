62953ceb5b8631fefd7cbda4e7508837b18785b6
AlanFnz <alan.f@msn.com>
2022-05-25 19:32:30 -0300

    created hash user password middleware

--- users/middleware/users.middleware.ts
+++ users/middleware/users.middleware.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import debug from "debug";
+import argon2 from "argon2";
 
 import userService from "../services/users.service";
 
@@ -90,7 +91,7 @@ class UsersMiddleware {
         errors: [ResponseMessages.INVALID_ID],
       });
     }
-    
+
     req.body._id = getObjectId(req.params.userId.toString());
     next();
   }
@@ -138,6 +139,29 @@ class UsersMiddleware {
         throw new APIError(ResponseMessages.USER_LAST_LOGIN_UPDATE_ERROR);
       }
     }
+  }
+
+  async hashUserPassword(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) {
+    if (req.body.password) {
+      try {
+        req.body.password = await argon2.hash(req.body.password);
+      } catch (e) {
+        log(e);
+        res.status(HttpStatusCode.INTERNAL_SERVER).send({
+          errors: [ResponseMessages.USER_PASSWORD_HASHING_ERROR],
+        });
+        throw new APIError(ResponseMessages.USER_CREATE_FAIL);
+      }
+    } else {
+      return res.status(HttpStatusCode.BAD_REQUEST).send({
+        errors: [ResponseMessages.USER_PASSWORD_NOT_FOUND],
+      });
+    }
+    next();
   }
 }
 
