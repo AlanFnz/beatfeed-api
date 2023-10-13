4bdc7e94e7fa4389082595e3982d6e75a2f57e18
Alan Fernandez <alan.f@msn.com>
2023-10-13 14:14:50 -0300

    implemented services on controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,38 +7,50 @@ import {
   HttpStatus,
   Param,
   Post,
+  Put,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/user.dto';
-
-const users = [
-  { id: '1', username: 'mark morgan', email: 'mark@morgan.com' },
-  { id: '2', username: 'keith richards', email: 'keith@richards.com' },
-];
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get()
   async getUsers() {
+    const users = await this.usersService.getUsers();
     return { success: true, payload: users };
   }
 
   @Get(':id')
-  async getUser(@Param() params: any) {
-    const user = users.find((user) => user.id === params.id);
-    return user
-      ? { success: true, payload: user }
-      : { success: true, payload: null };
+  async getUser(@Param('id') id: string) {
+    const user = await this.usersService.findUserById(id);
+    return { success: true, payload: user };
   }
 
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto) {
-    const { username, email, password } = createUserDto;
-    return { success: true, payload: { username, email, password } };
+    const user = await this.usersService.createUser(
+      createUserDto.username,
+      createUserDto.email,
+      createUserDto.password,
+    );
+    return { success: true, payload: user };
+  }
+
+  @Put(':id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() updateData: Partial<CreateUserDto>,
+  ) {
+    const user = await this.usersService.updateUser(id, updateData);
+    return { success: true, payload: user };
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   async deleteUser(@Param('id') id: string) {
+    await this.usersService.deleteUser(id);
     return { success: true };
   }
 }
