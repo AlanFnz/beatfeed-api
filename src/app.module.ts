d6428d520b914d7e963e7c5856cfa4d25fdc4a1c
Alan Fernandez <alan.f@msn.com>
2023-10-12 22:52:29 -0300

    db config on root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { EventsModule } from './events/events.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule, EventsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'test',
+      password: 'test1234',
+      database: 'test',
+      entities: [User],
+      synchronize: true,
+    }),
+    ,
+    UsersModule,
+    EventsModule,
+  ],
   controllers: [],
   providers: [],
 })
