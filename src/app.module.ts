f96885ec8a4dc35f4359ec4d046e65e4ab7f7f27
Alan Fernandez <alan.f@msn.com>
2023-10-12 22:04:25 -0300

    implemented typorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { EventsModule } from './events/events.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, EventsModule],
+  imports: [TypeOrmModule.forRoot(), UsersModule, EventsModule],
   controllers: [],
   providers: [],
 })
