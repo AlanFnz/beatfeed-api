2ce9e4fb584cb99021bf3214e10bdfbd7a2d1cc7
Alan Fernandez <alan.f@msn.com>
2023-10-12 21:48:49 -0300

    wip users controller

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,5 +1,6 @@
 export class CreateUserDto {
   username: string;
+  password: string;
   email: string;
 }
 
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/user.dto';
+
+const users = [
+  { id: '1', username: 'mark morgan', email: 'mark@morgan.com' },
+  { id: '2', username: 'keith richards', email: 'keith@richards.com' },
+];
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  async getUsers() {
+    return { success: true, payload: users };
+  }
+
+  @Get(':id')
+  async getUser(@Param() params: any) {
+    const user = users.find((user) => user.id === params.id);
+    return { success: true, payload: user };
+  }
+
+  @Post()
+  async createUser(@Body() createUserDto: CreateUserDto) {
+    const { username, email, password } = createUserDto;
+    return { success: true, payload: { username, email, password } };
+  }
+}
