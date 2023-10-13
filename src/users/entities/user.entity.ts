afdf3029da689d9796c45335dbf9d88e9d6eecc9
Alan Fernandez <alan.f@msn.com>
2023-10-12 22:05:49 -0300

    created user entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', unique: true, length: 255 })
+  username: string;
+
+  @Column({ type: 'varchar', unique: true, length: 255 })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  password: string;
+}
