40b77e4f09cd39571faa552c9446baca3f2488f6
Alan Fernandez <alan.f@msn.com>
2023-10-13 12:57:56 -0300

    updated tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { CreateUserDto } from './dto/user.dto';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -14,5 +15,50 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('getUsers', () => {
+    it('should return an array of users', async () => {
+      const result = { success: true, payload: expect.any(Array) };
+      expect(await controller.getUsers()).toEqual(result);
+    });
+  });
+
+  describe('getUser', () => {
+    it('should return a single user', async () => {
+      const result = { success: true, payload: expect.any(Object) };
+      expect(await controller.getUser({ id: '1' })).toEqual(result);
+    });
+
+    it('should return null if user is not found', async () => {
+      const result = { success: true, payload: null };
+      expect(await controller.getUser({ id: '10' })).toEqual(result);
+    });
+  });
+
+  describe('createUser', () => {
+    it('should create a user', async () => {
+      const createUserDto: CreateUserDto = {
+        username: 'test',
+        email: 'test@test.com',
+        password: 'test123',
+      };
+      const result = {
+        success: true,
+        payload: {
+          username: 'test',
+          email: 'test@test.com',
+          password: 'test123',
+        },
+      };
+      expect(await controller.createUser(createUserDto)).toEqual(result);
+    });
+  });
+
+  describe('deleteUser', () => {
+    it('should return success when deleting a user', async () => {
+      const result = { success: true };
+      expect(await controller.deleteUser('1')).toEqual(result);
+    });
   });
 });
