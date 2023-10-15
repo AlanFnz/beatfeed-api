85bd1254200217970a6682aa90a0e05e80f6d001
Alan Fernandez <alan.f@msn.com>
2023-10-15 12:44:07 -0300

    added and implemented validation pipeline

--- src/users/dto/user.dto.ts
+++ src/users/dto/user.dto.ts
@@ -1,13 +1,36 @@
+import { IsEmail, IsOptional, IsString, Length, IsUUID } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @Length(3, 50)
   username: string;
+
+  @IsString()
+  @Length(8, 100)
   password: string;
+
+  @IsEmail()
   email: string;
+
+  @IsOptional()
+  @IsString()
   profilePicture?: string;
 }
 
 export class UpdateUserDto {
+  @IsUUID()
   id: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(3, 50)
   username?: string;
+
+  @IsOptional()
+  @IsEmail()
   email?: string;
+
+  @IsOptional()
+  @IsString()
   profilePicture?: string;
 }
